701739aab5768ab11db0a528d5f05fa4d2d32a4d
Ebrahim soliman <soliman.ebrahim.dev@gmail.com>
2024-05-21 10:00:32 +0300

    update example

--- feedback/example/lib/main.dart
+++ feedback/example/lib/main.dart
@@ -1,20 +1,8 @@
-import 'dart:convert';
-import 'dart:developer';
 import 'dart:io';
-import 'dart:typed_data';
 
-import 'package:cross_file/cross_file.dart';
-import 'package:example/feedback_functions.dart';
 import 'package:feedback/feedback.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
-import 'package:flutter_email_sender/flutter_email_sender.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
-import 'package:http/http.dart' as http;
-import 'package:path_provider/path_provider.dart';
-import 'package:share_plus/share_plus.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -57,12 +45,7 @@ class _MyAppState extends State<MyApp> {
           Colors.yellow,
         ],
       ),
-      localizationsDelegates: const [
-        GlobalFeedbackLocalizationsDelegate(),
-        GlobalMaterialLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
+
       localeOverride: const Locale('en'),
       mode: FeedbackMode.draw,
       pixelRatio: 1,
@@ -95,46 +78,30 @@ class MyHomePage extends StatelessWidget {
           child: Column(
             children: <Widget>[
               const SizedBox(height: 10),
-              const MarkdownBody(
-                data: '# How does it work?\n'
-                    '1. Just press the `Provide feedback` button.\n'
-                    '2. The feedback view opens. '
-                    'You can choose between draw and navigation mode. '
-                    'When in navigate mode, you can freely navigate in the '
-                    'app. Try it by opening the navigation drawer or by '
-                    'tapping the `Open scaffold` button. To switch to the '
-                    'drawing mode just press the `Draw` button on the right '
-                    'side. Now you can draw on the screen.\n'
-                    '3. To finish your feedback just write a message '
-                    'below and send it by pressing the `Submit` button.',
+              const Text(
+                '# How does it work?\n'
+                '1. Just press the `Provide feedback` button.\n'
+                '2. The feedback view opens. '
+                'You can choose between draw and navigation mode. '
+                'When in navigate mode, you can freely navigate in the '
+                'app. Try it by opening the navigation drawer or by '
+                'tapping the `Open scaffold` button. To switch to the '
+                'drawing mode just press the `Draw` button on the right '
+                'side. Now you can draw on the screen.\n'
+                '3. To finish your feedback just write a message '
+                'below and send it by pressing the `Submit` button.',
               ),
               const SizedBox(height: 10),
               ElevatedButton(
                 child: const Text('Open scaffold'),
-                onPressed: () {
-                  Navigator.push<void>(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return _SecondaryScaffold();
-                      },
-                    ),
-                  );
-                },
+                onPressed: () {},
               ),
               const Divider(),
               ElevatedButton(
                 child: const Text('Provide feedback'),
                 onPressed: () {
                   BetterFeedback.of(context).show(
-                    (feedback) async {
-                      // upload to server, share whatever
-                      // for example purposes just show it to the user
-                      alertFeedbackFunction(
-                        context,
-                        feedback,
-                      );
-                    },
+                    (feedback) async {},
                   );
                 },
               ),
@@ -143,20 +110,7 @@ class MyHomePage extends StatelessWidget {
                 TextButton(
                   child: const Text('Provide E-Mail feedback'),
                   onPressed: () {
-                    BetterFeedback.of(context).show((feedback) async {
-                      // draft an email and send to developer
-                      final screenshotFilePath =
-                          await writeImageToStorage(feedback.screenshot);
-
-                      final Email email = Email(
-                        body: feedback.text,
-                        subject: 'App Feedback',
-                        recipients: ['john.doe@flutter.dev'],
-                        attachmentPaths: [screenshotFilePath],
-                        isHTML: false,
-                      );
-                      await FlutterEmailSender.send(email);
-                    });
+                    BetterFeedback.of(context).show((feedback) async {});
                   },
                 ),
                 const SizedBox(height: 10),
@@ -165,15 +119,7 @@ class MyHomePage extends StatelessWidget {
                 child: const Text('Provide feedback via platform sharing'),
                 onPressed: () {
                   BetterFeedback.of(context).show(
-                    (feedback) async {
-                      final screenshotFilePath =
-                          await writeImageToStorage(feedback.screenshot);
-
-                      await Share.shareXFiles(
-                        [XFile(screenshotFilePath)],
-                        text: feedback.text,
-                      );
-                    },
+                    (feedback) async {},
                   );
                 },
               ),
@@ -182,9 +128,7 @@ class MyHomePage extends StatelessWidget {
               const SizedBox(height: 10),
               ElevatedButton(
                 child: const Text('Visit library page on pub.dev'),
-                onPressed: () {
-                  openLink('https://pub.dev/packages/feedback');
-                },
+                onPressed: () {},
               ),
             ],
           ),
@@ -205,78 +149,4 @@ class MyHomePage extends StatelessWidget {
       ),
     );
   }
-
-  void openLink(String address) async {
-    final Uri url = Uri.parse(address);
-    if (!await launchUrl(url)) {
-      // Handle the error or notify the user
-      log('Could not launch $url');
-    }
-  }
-}
-
-class _SecondaryScaffold extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Scaffold 2'),
-      ),
-      body: const Center(
-        child: Text('Hello World'),
-      ),
-    );
-  }
-}
-
-Future<String> writeImageToStorage(Uint8List feedbackScreenshot) async {
-  final Directory output = await getTemporaryDirectory();
-  final String screenshotFilePath = '${output.path}/feedback.png';
-  final File screenshotFile = File(screenshotFilePath);
-  await screenshotFile.writeAsBytes(feedbackScreenshot);
-  return screenshotFilePath;
-}
-
-Future<void> createGitlabIssueFromFeedback(BuildContext context) async {
-  BetterFeedback.of(context).show((feedback) async {
-    const projectId = 'your-gitlab-project-id';
-    const apiToken = 'your-gitlab-api-token';
-
-    final screenshotFilePath = await writeImageToStorage(feedback.screenshot);
-
-    // Upload screenshot
-    final uploadRequest = http.MultipartRequest(
-      'POST',
-      Uri.https(
-        'gitlab.com',
-        '/api/v4/projects/$projectId/uploads',
-      ),
-    )
-      ..files.add(await http.MultipartFile.fromPath(
-        'file',
-        screenshotFilePath,
-      ))
-      ..headers.putIfAbsent('PRIVATE-TOKEN', () => apiToken);
-    final uploadResponse = await uploadRequest.send();
-
-    final dynamic uploadResponseMap = jsonDecode(
-      await uploadResponse.stream.bytesToString(),
-    );
-
-    // Create issue
-    await http.post(
-      Uri.https(
-        'gitlab.com',
-        '/api/v4/projects/$projectId/issues',
-        <String, String>{
-          'title': feedback.text.padRight(80),
-          'description': '${feedback.text}\n'
-              "${uploadResponseMap["markdown"] ?? "Missing image!"}",
-        },
-      ),
-      headers: {
-        'PRIVATE-TOKEN': apiToken,
-      },
-    );
-  });
 }
